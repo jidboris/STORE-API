a063935dc167b43bc2a5d187ff9268676ee4b5b5
Olajide Borisade <102518345+jidboris@users.noreply.github.com>
2022-08-22 23:07:57 +0100

    added getAllProduct route

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,37 +1,17 @@
 const Products = require("../module/productModel");
 // get controller for diplaying pages
 const getAllProducts = async (req,res,next) => {
-            const page = parseInt(req.query.page);
-            const limit = parseInt(req.query.limit);
-            const startIndex = (page - 1) * limit;
-            const endIndex = page * limit;
-
-            const result = {};
-            if (endIndex < (await Products.countDocuments().exec())) {
-                result.next = {
-                    page: page + 1,
-                    limit: limit,
-                };
-            }
-            if (startIndex > 0) {
-                result.previous = {
-                    page: page - 1,
-                    limit: limit,
-                };
-            }
-           try {
-                result.results = await Products.find().limit(limit).skip(startIndex);
-                res.paginatedResult = result;
-                next();
-} catch (e) {
-                res.status(500).json({ message: e.message });
-            }
-        };
-
-    // app.get('/api/v1/products', paginate(Products), (req, res) => {
-    //     console.log(res.paginatedResult)
-    //     res.json(res.paginatedResult)
-    // });
+            const {page,limit} = req.query;
+const product = await Products.find({})
+ .limit(limit)
+ .skip((page-1)*limit)
+ const count = await Products.countDocuments()
+ res.status(200).json({
+    Result: product,
+totalPage: Math.ceil(count/limit),
+currentPage : page
+});
+}
 
 // This get controller for filtered product
 const getAllProductsStatic = async (req, res, next) => {
